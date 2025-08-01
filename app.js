5368f439184294b18320448339259107cb922101
Ammad Ahmed <63580683+ammad-ahmed1@users.noreply.github.com>
2025-08-02 04:44:16 +0500

    express

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const http = require("http");
+const bodyParser = require("body-parser");
 
-const routes = require("./routes");
+const express = require("express");
 
-const server = http.createServer(routes);
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.sendFile(path.join(rootDir, "views", "404"));
+});
+
+const server = http.createServer(app);
+
 server.listen(3001);
