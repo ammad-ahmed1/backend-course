f817512c61bdfb8c86a358318b15b2195f4fbb91
Ammad Ahmed <63580683+ammad-ahmed1@users.noreply.github.com>
2025-08-02 22:40:59 +0500

    section-5 completed, learned  serving html pages with design in that

--- app.js
+++ app.js
@@ -6,17 +6,23 @@ const express = require("express");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
+const path = require("path");
+
 const app = express();
 
+const rootDir = require("./utils/path");
+
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "404"));
+  res.sendFile(path.join(rootDir, "views", "404.html"));
 });
 
 const server = http.createServer(app);
 
-server.listen(3001);
+server.listen(3000);
