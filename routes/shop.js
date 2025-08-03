e80ec4442add6d840fce60e48f053ea1749a43e8
Ammad Ahmed <63580683+ammad-ahmed1@users.noreply.github.com>
2025-08-04 00:50:23 +0500

    tempating engines to show dyamic data in views

--- routes/shop.js
+++ routes/shop.js
@@ -5,9 +5,19 @@ const path = require("path");
 const router = express.Router();
 
 const rootDir = require("../utils/path");
+const adminData = require("./admin");
 
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  console.log(adminData?.products, "products");
+  // res.sendFile(path.join(rootDir, "views", "shop.html"));
+  const products = adminData?.products;
+  res.render("shop", {
+    pageTitle: "Shop",
+    activePage: "shop",
+    extraCss: ["/css/product.css"],
+    prods: products,
+    docTitle: "Shop",
+  });
 });
 
 module.exports = router;
