22c75709991fa4202953a1224640623f153333ac
Ammad Ahmed <63580683+ammad-ahmed1@users.noreply.github.com>
2025-08-22 05:08:11 +0500

    error-handling done and validation in progress

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,54 @@
 const express = require("express");
-
+const { check, body } = require("express-validator");
 const authController = require("../controllers/auth");
-
 const router = express.Router();
+
 router.get("/signup", authController.getSignup);
-router.post("/signup", authController.postSignup);
+router.post(
+  "/signup",
+  check("email")
+    .isEmail()
+    .withMessage("Please enter a valid email.")
+    .custom((value, { req }) => {
+      if (value === "test@test.com") {
+        throw new Error("This email is forbidden!");
+      }
+      return true;
+    }),
+  body(
+    "confirmPassword",
+    "Please enter an alphanumeric password with atleast length of 5 characters"
+  )
+    .isLength({ min: 5 })
+    .isAlphanumeric(),
+  body("confirmPassword").custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error("Password have to match!");
+    }
+    return true;
+  }),
+  authController.postSignup
+);
 router.get("/login", authController.getLogin);
-router.post("/login", authController.postLogin);
+router.post(
+  "/login",
+  check("email")
+    .isEmail()
+    .withMessage("Please enter a valid email.")
+    .custom((value, { req }) => {
+      if (value === "test@test.com") {
+        throw new Error("This email is forbidden!");
+      }
+      return true;
+    }),
+  body(
+    "password",
+    "Please enter an alphanumeric password with atleast length of 5 characters"
+  )
+    .isLength({ min: 5 })
+    .isAlphanumeric(),
+  authController.postLogin
+);
 router.post("/logout", authController.postLogout);
 router.get("/reset", authController.getReset);
 router.post("/reset", authController.postReset);
